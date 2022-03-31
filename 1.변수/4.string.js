31d74c42da5717b83277a9038baa24f17e979040
곽민지 <mini0006@paiptree.com>
2022-03-31 15:43:51 +0900

    4강 완료

---
+++ 1.변수/4.string.js
@@ -0,0 +1,22 @@
+// 문자열 타입
+let string = '안녕하세요';
+string = `안녕!`;
+console.log(string);
+
+// 특수 문자 출력하는 법
+string = '"안녕!"';
+console.log(string);
+string = "'안녕?'";
+console.log(string);
+
+string = '안녕!\n엘리야!\t\t내 이름은\\'; // 이스케이프 표현 (MDN 확인)
+console.log(string);
+
+// 템플릿 리터럴 (Template Literal) ``
+let id = '엘리';
+let greetings = "'안녕!, " + id + "🤚\n즐거운 하루 보내요!'";
+console.log(greetings);
+
+greetings = `'안녕, ${id}🤚
+즐거운 하루 보내요!'`
+console.log(greetings);
---
+++ 1.변수/9.const.js
@@ -0,0 +1,26 @@
+// let 재할당이 가능
+let a = 1;
+a = 2;
+
+// const 재할당이 불가능
+// 1. 상수
+// 2. 상수변수 또는 변수
+const text = 'hello';
+// text = 'hi'; 이렇게 하면 안됨!
+
+// 1. 상수
+const MAX_FRUITS = 3;
+
+// 2. 재할당 불가능한 상수변수 또는 변수
+const apple = {
+    name: 'apple',
+    color: 'red',
+    display: '🇺🍎'
+};
+// apple = {};
+console.log(apple);
+apple.name = 'orange';
+console.log(apple);
+
+// let    재할당 O 변경 O
+// const  재할당 X 변경 O
