367538263f2067d467e773d0781b36c50d66f743
곽민지 <mini0006@paiptree.com>
2022-04-01 11:29:13 +0900

    5강 완료

---
+++ 2.operator/8.equality.js
@@ -0,0 +1,31 @@
+// 동등 비교 관계 연산자 (Equality Operators)
+// == 값이 같음
+// != 값이 다름
+// === 값과 타입이 둘 다 같음
+// !== 값과 타입이 다름
+console.log(2 == 2);
+console.log(2 != 2);
+console.log(2 != 3);
+console.log(2 == 3);
+console.log(2 == '2');
+console.log(2 === '2'); // ✨
+console.log(true == 1);
+console.log(true === 1);
+console.log(false == 0);
+console.log(false === 0);
+console.clear();
+
+const obj1 = {
+  name: 'js',
+};
+const obj2 = {
+  name: 'js',
+};
+
+console.log(obj1 == obj2); // 다른 메모리 주소!
+console.log(obj1 === obj2);
+console.log(obj1.name === obj2.name);
+
+let obj3 = obj2;
+console.log(obj3 == obj2);
+console.log(obj3 === obj2);
