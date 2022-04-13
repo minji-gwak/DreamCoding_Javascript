4449bd6d58a8d33dfd2c3a23d4b109742501b2fb
곽민지 <mini0006@paiptree.com>
2022-04-13 14:31:23 +0900

    9강 클래스 완료

--- 6.class/7.quiz2.js
+++ 6.class/7.quiz2.js
@@ -1,8 +1,8 @@
-// 정직원과 파트타임직원을 나타낼 수 있는 클래스를 만들어보자
-// 직원들의 정보: 이름, 부서이름, 한 달 근무시간
-// 매달 직원들의 정보를 이용해서 한 달 월급을 계산할 수 있다
-// 정직원은 시간당 10,000원
-// 파트타임 직원은 시간 당 8,000원
+// 정직원과 파트타임직원을 나타낼 수 있는 클래스를 만들어 보자
+// 직원들의 정보: 이름, 부서이름, 한달 근무 시간
+// 매달 직원들의 정보를 이용해서 한달 월급을 계산할 수 있다
+// 정직원은 시간당 10000원
+// 파트타임 직원은 시간당 8000원
 
 class Employee {
   constructor(name, department, hoursPerMonth, payRate) {
@@ -17,20 +17,19 @@ class Employee {
 }
 
 class FullTimeEmployee extends Employee {
-  static #PAY_RATE = 10000;
+  static PAY_RATE = 10000;
   constructor(name, department, hoursPerMonth) {
-    super(name, department, hoursPerMonth, FullTimeEmployee.#PAY_RATE);
+    super(name, department, hoursPerMonth, FullTimeEmployee.PAY_RATE);
   }
 }
-
 class PartTimeEmployee extends Employee {
-  static #PAY_RATE = 8000;
+  static PAY_RATE = 8000;
   constructor(name, department, hoursPerMonth) {
-    super(name, department, hoursPerMonth, PartTimeEmployee.#PAY_RATE);
+    super(name, department, hoursPerMonth, PartTimeEmployee.PAY_RATE);
   }
 }
 
-const fullTimeEmployee = new FullTimeEmployee('곽민지', '플랫폼개발', '140');
-const partTimeEmployee = new PartTimeEmployee('곽민지', '플랫폼개발', '140');
-console.log(fullTimeEmployee.calculatePay());
-console.log(partTimeEmployee.calculatePay());
+const ellie = new FullTimeEmployee('엘리', 's/w', 30);
+const bob = new PartTimeEmployee('밥', 's/w', 20);
+console.log(ellie.calculatePay());
+console.log(bob.calculatePay());
