b4258824e8299f4ec72491b98dc4c27bcad7382a
곽민지 <mini0006@paiptree.com>
2022-04-21 14:30:28 +0900

    refactor : prettier 파일 생성 및 적용

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+	trailingComma: 'es5',
+	tabWidth: 4,
+	printWidth: 120,
+	semi: true,
+	singleQuote: true,
+	bracketSpacing: true,
+	arrowParens: 'always',
+	proseWrap: 'preserve',
+	htmlWhitespaceSensitivity: 'strict',
+	endOfLine: 'lf',
+	useTabs: true,
+	jsxBracketSameLine: true,
+};
