0f3f255e6a0acc17c5883e5486b9e3c7d3ce0b54
곽민지 <mini0006@paiptree.com>
2022-04-21 14:29:19 +0900

    feat : 10강 빌트인 오브젝트 완료

--- 7.built-in/4.number.js
+++ 7.built-in/4.number.js
@@ -33,13 +33,13 @@ console.log(num4.toPrecision(4));
 console.log(num4.toPrecision(2)); // 전체 자릿수 표기가 안될때는 지수표기법
 
 if (Number.EPSILON > 0 && Number.EPSILON < 1) {
-  console.log(Number.EPSILON); // 0과 1사이에서 나타낼 수 있는 가장 작은 숫자
+	console.log(Number.EPSILON); // 0과 1사이에서 나타낼 수 있는 가장 작은 숫자
 }
 const num = 0.1 + 0.2 - 0.2; // 0.1
 console.log(num);
 
 function isEqual(original, expected) {
-  return Math.abs(original - expected) < Number.EPSILON;
+	return Math.abs(original - expected) < Number.EPSILON;
 }
 console.log(isEqual(1, 1));
 console.log(isEqual(0.1, 0.1));
--- 7.built-in/8.quiz.js
+++ 7.built-in/8.quiz.js
@@ -8,7 +8,7 @@ const text = 'Hello World!';
 // ..
 // !
 for (i = 0; i < text.length; i++) {
-  console.log(text[i]);
+	console.log(text[i]);
 }
 
 // 2. 사용자들의 id를 잘라내어 각각의 id를 배열로 보관
@@ -19,15 +19,15 @@ console.log(id);
 
 // 3. 1초에 한번씩 시계를 (날짜포함) 출력해보자
 function showTime() {
-  const now = new Date();
-  console.log(
-    now.getFullYear() + '년',
-    now.getMonth() + 1 + '월',
-    now.getDate() + '일',
-    now.getHours() + '시',
-    now.getMinutes() + '분',
-    now.getSeconds() + '초'
-  );
-  // console.log(now.toLocaleString('ko-KR'));
+	const now = new Date();
+	console.log(
+		now.getFullYear() + '년',
+		now.getMonth() + 1 + '월',
+		now.getDate() + '일',
+		now.getHours() + '시',
+		now.getMinutes() + '분',
+		now.getSeconds() + '초'
+	);
+	// console.log(now.toLocaleString('ko-KR'));
 }
 setInterval(showTime, 1000);
